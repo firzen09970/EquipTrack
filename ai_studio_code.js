fd9118ea163cf2a9941820bf22bdfba3b2e243c4
firzen09970 <53138241+firzen09970@users.noreply.github.com>
2026-01-27 01:10:50 +0800

    Add files via upload

---
+++ ai_studio_code.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'equip-control-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=SF+Pro+Display:wght@400;500;600;700&family=Noto+Sans+TC:wght@400;500;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
